fd9e5a03d52a0b3d7271c56ae39311b68ef9884a
Castiant02 <antonio.castillo03@utrgv.edu>
2025-02-02 02:06:25 +0000

    done

--- src/Converter.java
+++ src/Converter.java
@@ -1,1 +1,15 @@
+public class datacon{
+    public static void(String [] args){
+        String strfloat = "374.25";
+        float from_strfloat= Float.parseFloat(strfloat);
+        String strint = ="1250";
+        int from_strint=Integer.parseInt(strint);
+        float floatnum=56.715f;
+        int from_floatnum=(int)floatnum;
+        int intnum = 1500;
+        float from_intnum = (float)intnum;
+        double doublenum=3.14159265;
+        String from_doublenum = Double.toString(doublenum);
 
+    }
+}
